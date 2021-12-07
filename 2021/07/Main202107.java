227396a5d78b1659a30bf305bd0a616fed63b1b8
zfazek <ongroa@gmail.com>
2021-12-07 22:49:04 +0100

    refactor 2021 07 java solution

--- 2021/07/Main202107.java
+++ 2021/07/Main202107.java
@@ -1,41 +1,27 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main202107 {
 	public static void main(String[] args) throws IOException {
-		String input = Files.readString(Paths.get("2021/07/07.txt"));
-		one(input);
-		two(input);
-	}
-
-	private static void one(String input) {
-		String[] tokens = input.split(",");
-		List<Integer> results = new ArrayList<>();
-		for (int i = 0; i < tokens.length; i++) {
-			int value = Integer.parseInt(tokens[i]);
-			int s = Stream.of(tokens).mapToInt(Integer::valueOf).map(e -> Math.abs(e - value)).sum();
-			results.add(s);
-		}
-		System.out.println(Collections.min(results));
-	}
-
-	private static void two(String input) {
-		String[] tokens = input.split(",");
-		List<Integer> numbers = new ArrayList<>();
-		for (int i = 0; i < tokens.length; i++) {
-			numbers.add(Integer.parseInt(tokens[i]));
-		}
-		List<Integer> results = new ArrayList<>();
-		for (int i = Collections.min(numbers); i <= Collections.max(numbers); i++) {
+		List<Integer> input = Stream.of(Files.readString(Paths.get("2021/07/07.txt")).split(","))
+				.mapToInt(Integer::valueOf)
+				.sorted()
+				.mapToObj(Integer::valueOf)
+				.collect(Collectors.toList());
+		int min1 = Integer.MAX_VALUE;
+		int min2 = Integer.MAX_VALUE;
+		for (int i = input.get(0); i <= input.get(input.size() - 1); i++) {
 			int value = i;
-			int s = numbers.stream().mapToInt(Integer::valueOf).map(e -> Math.abs(e - value) * (Math.abs(e - value) + 1) / 2).sum();
-			results.add(s);
+			int s1 = input.stream().map(e -> Math.abs(e - value)).mapToInt(Integer::valueOf).sum();
+			int s2 = input.stream().map(e -> Math.abs(e - value) * (Math.abs(e - value) + 1) / 2).mapToInt(Integer::valueOf).sum();
+			min1 = Math.min(s1, min1);
+			min2 = Math.min(s2, min2);
 		}
-		System.out.println(Collections.min(results));
+		System.out.println(min1);
+		System.out.println(min2);
 	}
 }
