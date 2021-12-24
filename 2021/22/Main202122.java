6d9b782328a5103a16a3e2b5c1532c68ad93ec5c
zfazek <ongroa@gmail.com>
2021-12-24 12:46:53 +0100

    Revert "fix 2021 20 c++ solution"
    This reverts commit b06b583c18f9a9541c66514d8372d6d8b16f60bf.

--- 2021/22/Main202122.java
+++ 2021/22/Main202122.java
@@ -1,19 +1,11 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class Main202122 {
 	public static void main(String[] args) throws IOException {
-		List<String> input = Files.readAllLines(Paths.get("2021/22/22aa.txt"));
-		//		one(input);
-		two(input);
-	}
-
-	@SuppressWarnings("unused")
-	private static void one(List<String> input) {
+		List<String> input = Files.readAllLines(Paths.get("2021/22/22.txt"));
 		final int N = 50;
 		final int SIZE = 2 * N + 1;
 		int[][][] arr = new int[SIZE][SIZE][SIZE];
@@ -58,67 +50,5 @@ public class Main202122 {
 			}
 		}
 		System.out.println(n);
-	}
-
-	private static void two(List<String> input) {
-		List<Pos> positions = new ArrayList<>();
-		for (String line : input) {
-			System.out.println(line);
-			String[] tokens = line.split(" ");
-			String cmd = tokens[0];
-			String[] coords = tokens[1].split(",");
-			int x1 = Integer.parseInt(coords[0].split("=")[1].split("\\.\\.")[0]);
-			int x2 = Integer.parseInt(coords[0].split("=")[1].split("\\.\\.")[1]);
-			//			int y1 = Integer.parseInt(coords[1].split("=")[1].split("\\.\\.")[0]);
-			//			int y2 = Integer.parseInt(coords[1].split("=")[1].split("\\.\\.")[1]);
-			//			int z1 = Integer.parseInt(coords[2].split("=")[1].split("\\.\\.")[0]);
-			//			int z2 = Integer.parseInt(coords[2].split("=")[1].split("\\.\\.")[1]);
-			if (cmd.equals("on") && positions.isEmpty()) {
-				positions.add(new Pos(x1, x2));
-				continue;
-			}
-			if (cmd.equals("off") && positions.isEmpty()) {
-				continue;
-			}
-			ListIterator<Pos> it = positions.listIterator();
-			while (it.hasNext()) {
-				if (cmd.equals("on")) {
-					Pos p = it.next();
-					if ((x1 >= p.from && x1 <= p.end && x2 >= p.end) ||
-							(x1 <= p.from && x2 >= p.from && x2 <= p.end)) {
-						p.from = Math.min(x1, p.from);
-						p.end = Math.max(x2, p.end);
-						continue;
-					}
-					if (x2 <= p.from || x1 >= p.end) {
-						it.add(new Pos(x1, x2));
-						continue;
-					}
-				} else {
-
-				}
-			}
-		}
-		long n = 0;
-		for (Pos p : positions) {
-			System.out.println(p);
-			n += p.end - p.from + 1;
-		}
-		System.out.println(n);
-	}
-}
-
-class Pos {
-	int from;
-	int end;
-
-	Pos(int from, int end) {
-		this.from = from;
-		this.end = end;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("%d %d", from, end);
 	}
 }
