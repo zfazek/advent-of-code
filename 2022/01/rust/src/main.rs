c53f1a3962a68ce3967c70be510a5c897669ecfc
zfazek <ongroa@gmail.com>
2022-12-08 14:04:21 +0100

    update 2022 01 rust solution

--- 2022/01/rust/src/main.rs
+++ 2022/01/rust/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let mut sums: Vec<i64> = std::fs::read_to_string("../01.txt")
+    let mut sums: Vec<i32> = std::fs::read_to_string("../01.txt")
         .unwrap()
         .split("\n\n")
-        .map(|l| l.lines().map(|line| line.parse::<i64>().unwrap()).sum())
+        .map(|l| l.lines().map(|line| line.parse::<i32>().unwrap()).sum())
         .collect();
-    sums.sort();
-    sums.reverse();
+    sums.sort_by(|a, b| b.cmp(a));
+    let ans2: i32 = sums.iter().take(3).sum();
     println!("{}", sums[0]);
-    println!("{}", sums[0] + sums[1] + sums[2]);
+    println!("{}", ans2);
 }
