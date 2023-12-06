a685e2d0d99d6b4d411dc755405d3ac3863562e8
zfazek <ongroa@gmail.com>
2023-12-06 08:42:12 +0100

    add 2023 06 rust solution

---
+++ 2023/06/rust/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let input = include_str!("../../input.txt");
+    println!("{}", foo1(input));
+    println!("{}", foo2(input));
+}
+
+fn foo1(input: &str) -> i64 {
+    let mut ans: i64 = 1;
+    let lines = input.lines().collect::<Vec<_>>();
+    let times = lines[0].split_ascii_whitespace().collect::<Vec<_>>();
+    let distances = lines[1].split_ascii_whitespace().collect::<Vec<_>>();
+    for i in 1..times.len() {
+        let time = times[i].parse::<i32>().unwrap();
+        let distance = distances[i].parse::<i32>().unwrap();
+        let mut count: i64 = 0;
+        for n in 1..time {
+            if (time - n) * n > distance {
+                count += 1;
+            }
+        }
+        ans *= count;
+    }
+    ans
+}
+
+fn foo2(input: &str) -> i64 {
+    let mut ans: i64 = 0;
+    let lines = input.lines().collect::<Vec<_>>();
+    let time = lines[0].split(':').nth(1).unwrap().replace(' ', "").parse::<i64>().unwrap();
+    let distance = lines[1].split(':').nth(1).unwrap().replace(' ', "").parse::<i64>().unwrap();
+    for n in 1..time {
+        if (time - n) * n > distance {
+            ans += 1;
+        }
+    }
+    ans
+}
