1a92bc6bcf1499a9e2d6918863657b5df56b3cee
zfazek <ongroa@gmail.com>
2023-12-16 12:15:03 +0100

    update 2023 16 rust solution

--- 2023/16/rust/src/main.rs
+++ 2023/16/rust/src/main.rs
@@ -1,46 +1,28 @@
 use std::collections::HashSet;
 
-mod my {
-    #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-    pub struct Point {
-        x: i32,
-        y: i32,
-        dx: i32,
-        dy: i32,
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct Point {
+    x: i32,
+    y: i32,
+    dx: i32,
+    dy: i32,
+}
+
+impl Point {
+    fn new_with(x: i32, y: i32, dx: i32, dy: i32) -> Self {
+        Point { x, y, dx, dy }
     }
 
-    impl Point {
-        pub fn new_with(x: i32, y: i32, dx: i32, dy: i32) -> Self {
-            Point { x, y, dx, dy }
-        }
+    fn mov(&mut self, dx: i32, dy: i32) {
+        self.dx = dx;
+        self.dy = dy;
+        self.x += self.dx;
+        self.y += self.dy;
+    }
 
-        pub fn x(&self) -> i32 {
-            self.x
-        }
-
-        pub fn y(&self) -> i32 {
-            self.y
-        }
-
-        pub fn dx(&self) -> i32 {
-            self.dx
-        }
-
-        pub fn dy(&self) -> i32 {
-            self.dy
-        }
-
-        pub fn mov(&mut self, dx: i32, dy: i32) {
-            self.dx = dx;
-            self.dy = dy;
-            self.x += self.dx;
-            self.y += self.dy;
-        }
-
-        pub fn reset(&mut self) {
-            self.dx = 0;
-            self.dy = 0;
-        }
+    fn reset(&mut self) {
+        self.dx = 0;
+        self.dy = 0;
     }
 }
 
@@ -54,7 +36,7 @@ fn main() {
     let w = vs[0].len() as i32;
     let h = vs.len() as i32;
     let mut start_positions = Vec::new();
-    start_positions.push(my::Point::new_with(0, 0, 1, 0));
+    start_positions.push(Point::new_with(0, 0, 1, 0));
     foo(&vs.clone(), &start_positions);
     start_positions.clear();
     for i in 0..h {
@@ -75,23 +57,23 @@ fn main() {
                 continue;
             }
             if i == 0 {
-                start_positions.push(my::Point::new_with(j, i, 0, 1));
+                start_positions.push(Point::new_with(j, i, 0, 1));
             }
             if i == h - 1 {
-                start_positions.push(my::Point::new_with(j, i, 0, -1));
+                start_positions.push(Point::new_with(j, i, 0, -1));
             }
             if j == 0 {
-                start_positions.push(my::Point::new_with(j, i, 1, 0));
+                start_positions.push(Point::new_with(j, i, 1, 0));
             }
             if j == w - 1 {
-                start_positions.push(my::Point::new_with(j, i, -1, 0));
+                start_positions.push(Point::new_with(j, i, -1, 0));
             }
         }
     }
     foo(&vs.clone(), &start_positions);
 }
 
-fn foo(v: &Vec<Vec<char>>, start_positions: &Vec<my::Point>) {
+fn foo(v: &Vec<Vec<char>>, start_positions: &Vec<Point>) {
     let mut ans = 0;
     let w = v[0].len() as i32;
     let h = v.len() as i32;
@@ -104,52 +86,36 @@ fn foo(v: &Vec<Vec<char>>, start_positions: &Vec<my::Point>) {
         while !beams.is_empty() {
             let mut new_beams = Vec::new();
             for beam in beams.iter_mut() {
-                let c = vs[beam.y() as usize][beam.x() as usize];
-                cells.insert((beam.y(), beam.x()));
+                let c = vs[beam.y as usize][beam.x as usize];
+                cells.insert((beam.y, beam.x));
                 match c {
                     '.' | '#' => {
-                        vs[beam.y() as usize][beam.x() as usize] = '#';
-                        beam.mov(beam.dx(), beam.dy());
+                        vs[beam.y as usize][beam.x as usize] = '#';
+                        beam.mov(beam.dx, beam.dy);
                     }
                     '-' => {
-                        if beam.dy() == 0 {
-                            beam.mov(beam.dx(), 0);
+                        if beam.dy == 0 {
+                            beam.mov(beam.dx, beam.dy);
                         } else {
-                            let new_beam = my::Point::new_with(beam.x() - 1, beam.y(), -1, 0);
+                            let new_beam = Point::new_with(beam.x - 1, beam.y, -1, 0);
                             new_beams.push(new_beam);
                             beam.mov(1, 0);
                         }
                     }
                     '|' => {
-                        if beam.dx() == 0 {
-                            beam.mov(0, beam.dy());
+                        if beam.dx == 0 {
+                            beam.mov(beam.dx, beam.dy);
                         } else {
-                            let new_beam = my::Point::new_with(beam.x(), beam.y() - 1, 0, -1);
+                            let new_beam = Point::new_with(beam.x, beam.y - 1, 0, -1);
                             new_beams.push(new_beam);
                             beam.mov(0, 1);
                         }
                     }
                     '/' => {
-                        if beam.dx() == 0 && beam.dy() == 1 {
-                            beam.mov(-1, 0);
-                        } else if beam.dx() == 0 && beam.dy() == -1 {
-                            beam.mov(1, 0);
-                        } else if beam.dx() == 1 && beam.dy() == 0 {
-                            beam.mov(0, -1);
-                        } else if beam.dx() == -1 && beam.dy() == 0 {
-                            beam.mov(0, 1);
-                        }
+                        beam.mov(-beam.dy, -beam.dx);
                     }
                     '\\' => {
-                        if beam.dx() == 0 && beam.dy() == 1 {
-                            beam.mov(1, 0);
-                        } else if beam.dx() == 0 && beam.dy() == -1 {
-                            beam.mov(-1, 0);
-                        } else if beam.dx() == 1 && beam.dy() == 0 {
-                            beam.mov(0, 1);
-                        } else if beam.dx() == -1 && beam.dy() == 0 {
-                            beam.mov(0, -1);
-                        }
+                        beam.mov(beam.dy, beam.dx);
                     }
                     _ => {}
                 }
@@ -163,13 +129,13 @@ fn foo(v: &Vec<Vec<char>>, start_positions: &Vec<my::Point>) {
                 beams.push(beam.clone());
             }
             for beam in beams.iter_mut() {
-                if beam.x() < 0 || beam.x() >= w || beam.y() < 0 || beam.y() >= h {
+                if beam.x < 0 || beam.x >= w || beam.y < 0 || beam.y >= h {
                     beam.reset();
                 }
             }
             new_beams.clear();
             for beam in beams {
-                if beam.dx() != 0 || beam.dy() != 0 {
+                if beam.dx != 0 || beam.dy != 0 {
                     new_beams.push(beam);
                 }
             }
