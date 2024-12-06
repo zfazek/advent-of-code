eb119aba17f492a2431b9b4198d213bda732d9ed
zfazek <ongroa@gmail.com>
2024-12-06 19:11:10 +0100

    update 2024 2 rust solution

--- 2024/02/rust/src/main.rs
+++ 2024/02/rust/src/main.rs
@@ -1,20 +1,19 @@
+use itertools::Itertools;
+
 fn main() {
     let input = std::fs::read_to_string("../input.txt").unwrap();
     let result1 = first(&input);
-    println!("{}", result1);
     let result2 = second(&input);
-    println!("{}", result2);
+    dbg!(result1, result2);
 }
 
 fn first(input: &str) -> i32 {
     let mut result = 0;
     for line in input.lines() {
-        let mut v = Vec::new();
         let tokens = line.split_ascii_whitespace();
-        for n in tokens {
-            let a = n.parse::<i32>().unwrap();
-            v.push(a);
-        }
+        let v = tokens
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
         if is_safe(&v) {
             result += 1;
         }
@@ -25,24 +24,21 @@ fn first(input: &str) -> i32 {
 fn second(input: &str) -> i32 {
     let mut result = 0;
     for line in input.lines() {
-        let mut v = Vec::new();
         let tokens = line.split_whitespace();
-        for n in tokens {
-            let a = n.parse::<i32>().unwrap();
-            v.push(a);
-        }
+        let v = tokens
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
         if is_safe(&v) {
             result += 1;
             continue;
         }
         for i in 0..v.len() {
-            let vv = v.clone();
-            v.remove(i);
-            if is_safe(&v) {
+            let mut vv = v.clone();
+            vv.remove(i);
+            if is_safe(&vv) {
                 result += 1;
                 break;
             }
-            v = vv;
         }
     }
     result
@@ -50,8 +46,8 @@ fn second(input: &str) -> i32 {
 
 fn is_safe(v: &Vec<i32>) -> bool {
     let sign = v[1] - v[0];
-    for i in 1..v.len() {
-        let d = v[i] - v[i - 1];
+    for (&a, &b) in v.iter().tuple_windows() {
+        let d = b - a;
         if sign > 0 {
             if !(1..=3).contains(&d) {
                 return false;
