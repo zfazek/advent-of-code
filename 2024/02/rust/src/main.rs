40cecf89a6fa8e581971d687b4174ac8a259cfd3
zfazek <ongroa@gmail.com>
2024-12-06 20:36:28 +0100

    update 2024 2 rust solution

--- 2024/02/rust/src/main.rs
+++ 2024/02/rust/src/main.rs
@@ -3,8 +3,9 @@ use itertools::Itertools;
 fn main() {
     let input = std::fs::read_to_string("../input.txt").unwrap();
     let result1 = first(&input);
+    println!("{result1}");
     let result2 = second(&input);
-    dbg!(result1, result2);
+    println!("{result2}");
 }
 
 fn first(input: &str) -> i32 {
@@ -45,16 +46,7 @@ fn second(input: &str) -> i32 {
 }
 
 fn is_safe(v: &Vec<i32>) -> bool {
-    let sign = v[1] - v[0];
-    for (&a, &b) in v.iter().tuple_windows() {
-        let d = b - a;
-        if sign > 0 {
-            if !(1..=3).contains(&d) {
-                return false;
-            }
-        } else if !(-3..=-1).contains(&d) {
-            return false;
-        }
-    }
-    true
+    let mut w = v.iter().tuple_windows();
+    let mut ww = w.clone();
+    w.all(|(&a, &b)| (1..=3).contains(&(a - b))) || ww.all(|(&a, &b)| (1..=3).contains(&(b - a)))
 }
