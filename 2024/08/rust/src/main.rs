389ffe83ae820bad7557185b59d2ddb0e21a46d1
zfazek <ongroa@gmail.com>
2024-12-08 20:20:36 +0100

    update 2024 8 rust solution

--- 2024/08/rust/src/main.rs
+++ 2024/08/rust/src/main.rs
@@ -4,7 +4,8 @@ use std::collections::HashMap;
 fn main() {
     let input = std::fs::read_to_string("../input.txt").unwrap();
     let mut dp: HashMap<char, BTreeSet<(i32, i32)>> = HashMap::new();
-    let mut positions = BTreeSet::new();
+    let mut positions1 = BTreeSet::new();
+    let mut positions2 = BTreeSet::new();
     for (i, line) in input.lines().enumerate() {
         for (j, c) in line.chars().enumerate() {
             if c != '.' {
@@ -16,40 +17,28 @@ fn main() {
     for e in dp.iter() {
         let antenna_positions = e.1;
         for p1 in antenna_positions.iter() {
-            for p2 in antenna_positions.iter() {
-                if p1 == p2 {
-                    continue;
-                }
-                let dir = (p2.0 - p1.0, p2.1 - p1.1);
-                let new_pos = (p2.0 + dir.0, p2.1 + dir.1);
-                if new_pos.0 < 0 || new_pos.0 >= size || new_pos.1 < 0 || new_pos.1 >= size {
-                    continue;
-                }
-                positions.insert(new_pos);
-            }
-        }
-    }
-    println!("{}", positions.len());
-    positions.clear();
-    for e in dp.iter() {
-        let antenna_positions = e.1;
-        for p1 in antenna_positions.iter() {
-            positions.insert(*p1);
+            positions2.insert(*p1);
             for p2 in antenna_positions.iter() {
                 if p1 == p2 {
                     continue;
                 }
                 let dir = (p2.0 - p1.0, p2.1 - p1.1);
                 let mut new_pos = (p2.0 + dir.0, p2.1 + dir.1);
+                let mut first = true;
                 loop {
                     if new_pos.0 < 0 || new_pos.0 >= size || new_pos.1 < 0 || new_pos.1 >= size {
                         break;
                     }
-                    positions.insert(new_pos);
+                    if first {
+                        positions1.insert(new_pos);
+                        first = false;
+                    }
+                    positions2.insert(new_pos);
                     new_pos = (new_pos.0 + dir.0, new_pos.1 + dir.1);
                 }
             }
         }
     }
-    println!("{}", positions.len());
+    println!("{}", positions1.len());
+    println!("{}", positions2.len());
 }
