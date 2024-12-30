d8144d12f63da220887e65301775a8b549378721
zfazek <ongroa@gmail.com>
2024-12-30 15:52:03 +0100

    add 2024 15 part 2 rust solution

--- 2024/15/rust/src/main.rs
+++ 2024/15/rust/src/main.rs
@@ -1,25 +1,25 @@
 use std::collections::BTreeMap;
-use std::collections::BTreeSet;
 
 struct Dir {
     x: i32,
     y: i32,
 }
 
+#[derive(Debug)]
 struct Robot {
     x: i32,
     y: i32,
 }
 
-#[derive(Ord, PartialOrd, PartialEq, Eq)]
+#[derive(Debug, Clone, Ord, PartialOrd, PartialEq, Eq)]
 struct Box {
+    y: i32,
     x1: i32,
     x2: i32,
-    y: i32,
 }
 
 fn main() {
-    let input = std::fs::read_to_string("../inputa.txt").unwrap();
+    let input = std::fs::read_to_string("../input.txt").unwrap();
     let mut dirs = BTreeMap::new();
     dirs.insert('<', Dir { x: -1, y: 0 });
     dirs.insert('>', Dir { x: 1, y: 0 });
@@ -35,8 +35,7 @@ fn first(input: &str, dirs: &BTreeMap<char, Dir>) {
     let mut robot = Robot { x: 0, y: 0 };
     for (i, line) in a.0.lines().enumerate() {
         let v = line.chars().collect::<Vec<_>>();
-        for j in 0..v.len() {
-            let c = v[j];
+        for (j, &c) in v.iter().enumerate() {
             if c == '@' {
                 robot.x = i as i32;
                 robot.y = j as i32;
@@ -44,7 +43,6 @@ fn first(input: &str, dirs: &BTreeMap<char, Dir>) {
         }
         vv.push(v);
     }
-    //_print(&vv, ' ');
     let mut moves = Vec::new();
     for line in a.1.lines() {
         for c in line.chars() {
@@ -52,8 +50,7 @@ fn first(input: &str, dirs: &BTreeMap<char, Dir>) {
         }
     }
     for m in moves {
-        mov1(&mut vv, m, &dirs, &mut robot);
-        //_print(&vv, m);
+        mov1(&mut vv, m, dirs, &mut robot);
     }
     let result1 = calculate_part1(vv);
     println!("{}", result1);
@@ -130,19 +127,14 @@ fn second(input: &str, dirs: &BTreeMap<char, Dir>) {
         vv.push(v);
     }
     let mut robot = Robot { x: 0, y: 0 };
-    let mut boxes = BTreeSet::new();
     for i in 0..vv.len() {
         for j in 0..vv[i].len() {
             if vv[i][j] == '@' {
-                robot.x = i as i32;
-                robot.y = j as i32;
-            } else if vv[i][j] == 'O' {
-                let b = Box {x1: i as i32, x2: (i + 1) as i32, y: j as i32};
-                boxes.insert(b);
+                robot.x = j as i32;
+                robot.y = i as i32;
             }
         }
     }
-    _print(&vv, ' ');
     let mut moves = Vec::new();
     for line in a.1.lines() {
         for c in line.chars() {
@@ -150,8 +142,8 @@ fn second(input: &str, dirs: &BTreeMap<char, Dir>) {
         }
     }
     for m in moves {
-        //mov2(&mut vv, m, &dirs, &mut robot);
-        //_print(&vv, m);
+        mov2(&mut vv, m, dirs, &mut robot);
+        //_print(&vv, m, &robot);
     }
     let result2 = calculate_part2(vv);
     println!("{}", result2);
@@ -159,64 +151,151 @@ fn second(input: &str, dirs: &BTreeMap<char, Dir>) {
 
 fn mov2(vv: &mut Vec<Vec<char>>, m: char, dirs: &BTreeMap<char, Dir>, robot: &mut Robot) {
     let dir = dirs.get(&m).unwrap();
-    let nx = (robot.x + dir.x) as usize;
-    let ny = (robot.y + dir.y) as usize;
+    let mut nx = robot.x + dir.x;
+    let ny = robot.y + dir.y;
     let x = robot.x as usize;
     let y = robot.y as usize;
-    let c = vv[ny][nx];
-    if c == '.' {
-        vv[y][x] = '.';
-        vv[ny][nx] = '@';
-        robot.x = nx as i32;
-        robot.y = ny as i32;
+    let c = vv[ny as usize][nx as usize];
+    if c == '#' {
         return;
-    } else if c == '#' {
+    } else if c == '.' {
+        vv[y][x] = '.';
+        vv[ny as usize][nx as usize] = '@';
+        robot.x = nx;
+        robot.y = ny;
         return;
     }
-    let mut k = 2;
-    loop {
-        let nx1 = (robot.x + k * dir.x) as usize;
-        let ny1 = (robot.y + k * dir.y) as usize;
-        let c = vv[ny1][nx1];
-        if c == '#' {
+    let mut moving_boxes = Vec::new();
+    if dir.y == 0 {
+        loop {
+            let c = vv[ny as usize][nx as usize];
+            if c == '[' {
+                let b = Box {
+                    x1: nx,
+                    x2: nx + 1,
+                    y: ny,
+                };
+                moving_boxes.push(b);
+            } else if c == ']' {
+                let b = Box {
+                    x1: nx - 1,
+                    x2: nx,
+                    y: ny,
+                };
+                moving_boxes.push(b);
+            } else if c == '#' || c == '.' {
+                break;
+            }
+            nx += dir.x * 2;
+        }
+        let collision = moving_boxes.iter().any(|b| {
+            vv[b.y as usize][(b.x1 + dir.x) as usize] == '#'
+                || vv[b.y as usize][(b.x2 + dir.x) as usize] == '#'
+        });
+        if collision {
             return;
         }
-        if c == '.' {
-            vv[y][x] = '.';
-            vv[ny][nx] = '@';
-            vv[ny1][nx1] = 'O';
-            robot.x = nx as i32;
-            robot.y = ny as i32;
+        for b in moving_boxes {
+            vv[b.y as usize][(b.x1 + dir.x) as usize] = '[';
+            vv[b.y as usize][(b.x2 + dir.x) as usize] = ']';
+        }
+    } else {
+        let c = vv[ny as usize][nx as usize];
+        if c == '[' {
+            let b = Box {
+                x1: nx,
+                x2: nx + 1,
+                y: ny,
+            };
+            moving_boxes.push(b);
+            foo(dir.y, nx, ny, &mut moving_boxes, vv);
+        } else if c == ']' {
+            let b = Box {
+                x1: nx - 1,
+                x2: nx,
+                y: ny,
+            };
+            moving_boxes.push(b);
+            foo(dir.y, nx - 1, ny, &mut moving_boxes, vv);
+        }
+        moving_boxes.sort();
+        if dir.y == 1 {
+            moving_boxes.reverse();
+        }
+        let collision = moving_boxes.iter().any(|b| {
+            vv[(b.y + dir.y) as usize][b.x1 as usize] == '#'
+                || vv[(b.y + dir.y) as usize][b.x2 as usize] == '#'
+        });
+        if collision {
             return;
         }
-        k += 1;
+        for b in moving_boxes {
+            vv[(b.y + dir.y) as usize][(b.x1 + dir.x) as usize] = '[';
+            vv[(b.y + dir.y) as usize][(b.x2 + dir.x) as usize] = ']';
+            vv[b.y as usize][(b.x1 + dir.x) as usize] = '.';
+            vv[b.y as usize][(b.x2 + dir.x) as usize] = '.';
+        }
+    }
+    robot.x += dir.x;
+    robot.y += dir.y;
+    vv[y][x] = '.';
+    vv[robot.y as usize][robot.x as usize] = '@';
+}
+
+fn foo(dir_y: i32, x: i32, y: i32, moving_boxes: &mut Vec<Box>, vv: &Vec<Vec<char>>) {
+    let a1 = vv[(y + dir_y) as usize][x as usize];
+    let a2 = vv[(y + dir_y) as usize][(x + 1) as usize];
+    if a1 == '[' {
+        let b = Box {
+            y: y + dir_y,
+            x1: x,
+            x2: x + 1,
+        };
+        moving_boxes.push(b);
+        foo(dir_y, x, y + dir_y, moving_boxes, vv);
+    }
+    if a1 == ']' {
+        let b = Box {
+            y: y + dir_y,
+            x1: x - 1,
+            x2: x,
+        };
+        moving_boxes.push(b);
+        foo(dir_y, x - 1, y + dir_y, moving_boxes, vv);
+    }
+    if a2 == '[' {
+        let b = Box {
+            y: y + dir_y,
+            x1: x + 1,
+            x2: x + 2,
+        };
+        moving_boxes.push(b);
+        foo(dir_y, x + 1, y + dir_y, moving_boxes, vv);
     }
 }
 
 fn calculate_part2(vv: Vec<Vec<char>>) -> usize {
     //dbg!(&vv);
     let mut result1 = 0;
-    let width = vv[0].len();
-    let height = vv.len(); 
     for i in 0..vv.len() {
         for j in 0..vv[i].len() {
             if vv[i][j] == '[' {
-                let x1 = j;
-                let x2 = width - (j + 1);
-                let y1 = i;
-                let y2 = height - (i + 1);
-                result1 += 100 * y1.min(y2) + x1.min(x2);
+                result1 += 100 * i + j;
             }
         }
     }
     result1
 }
 
-fn _print(vv: &Vec<Vec<char>>, m: char) {
+fn _print(vv: &Vec<Vec<char>>, m: char, robot: &Robot) {
     println!("{}", m);
-    for v in vv.iter() {
-        for c in v.iter() {
-            print!("{}", c);
+    for (i, v) in vv.iter().enumerate() {
+        for (j, &c) in v.iter().enumerate() {
+            if i == robot.y as usize && j == robot.x as usize {
+                print!("@");
+            } else {
+                print!("{}", c);
+            }
         }
         println!();
     }
