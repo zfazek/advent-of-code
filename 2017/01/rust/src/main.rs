50f44ee4ed08e5ef3ec57bcc47d8368a68081270
zfazek <ongroa@gmail.com>
2025-01-18 07:46:16 +0100

    update 2017 2 rust solution

--- 2017/01/rust/src/main.rs
+++ 2017/01/rust/src/main.rs
@@ -1,40 +1,34 @@
 fn main() {
-    let input: Vec<_> = std::fs::read_to_string("../01.txt")
-        .unwrap()
-        .lines()
-        .map(str::to_string)
-        .collect();
+    let input = std::fs::read_to_string("../01.txt").unwrap();
     one(&input);
     two(&input);
 }
 
-fn one(input: &Vec<String>) {
-    for line in input {
-        let mut sum = 0;
-        let chars: Vec<_> = line.chars().collect();
-        for (i, c) in line.chars().enumerate() {
+fn one(line: &String) {
+    let chars: Vec<_> = line.chars().collect();
+    let sum = line
+        .chars()
+        .enumerate()
+        .map(|(i, c)| {
             let idx = if i == chars.len() - 1 { 0 } else { i + 1 };
-            if c == chars[idx] {
-                sum += c.to_digit(10).unwrap();
-            }
-        }
-        println!("{}", sum);
-    }
+            (chars[idx], c)
+        })
+        .filter(|(cc, c)| c == cc)
+        .map(|c| c.0.to_digit(10).unwrap())
+        .sum::<u32>();
+    println!("{}", sum);
 }
 
-fn two(input: &Vec<String>) {
-    for line in input {
-        let mut sum = 0;
-        let chars: Vec<_> = line.chars().collect();
-        for (i, c) in line.chars().enumerate() {
-            if i == chars.len() / 2 {
-                break;
-            }
+fn two(line: &String) {
+    let chars: Vec<_> = line.chars().collect();
+    let sum = (0..chars.len() / 2)
+        .map(|i| {
+            let c = line.chars().nth(i).unwrap();
             let idx = i + chars.len() / 2;
-            if c == chars[idx] {
-                sum += 2 * c.to_digit(10).unwrap();
-            }
-        }
-        println!("{}", sum);
-    }
+            (chars[idx], c)
+        })
+        .filter(|(cc, c)| c == cc)
+        .map(|c| 2 * c.0.to_digit(10).unwrap())
+        .sum::<u32>();
+    println!("{}", sum);
 }
