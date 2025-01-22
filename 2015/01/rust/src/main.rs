bf061e6c2958fd2594a4cc6165b35a658ec9dac4
zfazek <ongroa@gmail.com>
2025-01-22 09:38:46 +0100

    update 2015 1 rust solution

--- 2015/01/rust/src/main.rs
+++ 2015/01/rust/src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
     let input = include_str!("../../01.txt");
+    imperative(input);
+    let vv = input.chars().collect::<Vec<_>>();
+    let n = foo(&vv, 0, 0);
+    let idx = foo1(&vv, 0, 0);
+    println!("{}", n);
+    println!("{}", idx);
+}
+
+fn imperative(input: &str) {
     let mut n = 0;
     let mut idx = 0;
     for line in input.lines() {
@@ -16,4 +25,26 @@ fn main() {
     }
     println!("{}", n);
     println!("{}", idx);
+}
+
+fn foo(vv: &Vec<char>, i: usize, count: i32) -> i32 {
+    if i == vv.len() {
+        return count;
+    }
+    if vv[i] == '(' {
+        return foo(vv, i + 1, count + 1);
+    } else {
+        return foo(vv, i + 1, count - 1);
+    }
+}
+
+fn foo1(vv: &Vec<char>, i: usize, count: i32) -> usize {
+    if vv[i] == '(' {
+        return foo1(vv, i + 1, count + 1);
+    } else {
+        if count < 0 {
+            return i;
+        }
+        return foo1(vv, i + 1, count - 1);
+    }
 }
