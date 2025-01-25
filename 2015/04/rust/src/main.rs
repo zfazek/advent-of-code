d497e672ca8d3617e9b38e6d7d60c0a1a322ba1c
zfazek <ongroa@gmail.com>
2025-01-25 05:37:00 +0100

    update 2015 4 rust solution

--- 2015/04/rust/src/main.rs
+++ 2015/04/rust/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let mut n:i64 = 0;
+    let mut n: i64 = 0;
     let key = "iwrupvqb";
-    let mut flag = false;
+    let mut first = false;
     loop {
         let plaintext = format!("{}{}", key, n);
         let s = md5::compute(&plaintext);
         let str = format!("{:x}", s);
-        if !flag && str.starts_with("00000") {
+        if !first && str.starts_with("00000") {
             println!("{}", n);
-            flag = true;
+            first = true;
         }
         if str.starts_with("000000") {
             println!("{}", n);
