f44a930e23c4e33ceaf44b23f9ef98abbd1ba761
zfazek <ongroa@gmail.com>
2025-01-30 08:11:22 +0100

    update 2019 1 rust solution

--- 2019/01/rust/src/main.rs
+++ 2019/01/rust/src/main.rs
@@ -1,37 +1,33 @@
-use std::fs;
-
-
 fn main() {
-    let input = fs::read_to_string("../01.txt").unwrap();
+    let input = std::fs::read_to_string("../01.txt").unwrap();
     one(&input);
     two(&input);
 }
 
-fn one(input: &String) {
-    let mut sum = 0;
-    for line in input.lines() {
-        if let Ok(n) = line.parse::<i32>() {
-            sum += n / 3 - 2;
-        }
-    }
+fn one(input: &str) {
+    let sum = input
+        .lines()
+        .filter_map(|line| line.parse::<i32>().ok())
+        .map(|n| n / 3 - 2)
+        .sum::<i32>();
     println!("{}", sum);
 }
 
-fn two(input: &String) {
-    let mut sum = 0;
-    for line in input.lines() {
-        let mut s = 0;
-        if let Ok(n) = line.parse::<i32>() {
+fn two(input: &str) {
+    let sum = input
+        .lines()
+        .filter_map(|line| line.parse::<i32>().ok())
+        .map(|n| {
             let mut m = n;
+            let mut s = 0;
             while m > 0 {
                 m = m / 3 - 2;
                 if m > 0 {
                     s += m;
                 }
             }
-            sum += s;
-        }
-    }
+            s
+        })
+        .sum::<i32>();
     println!("{}", sum);
 }
-
