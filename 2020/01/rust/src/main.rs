b3ebe65989b7ae35bcae2d496d76b90e324b7ff8
zfazek <ongroa@gmail.com>
2025-01-31 09:56:13 +0100

    update 2020 1 rust solution

--- 2020/01/rust/src/main.rs
+++ 2020/01/rust/src/main.rs
@@ -1,27 +1,26 @@
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 use std::fs::read_to_string;
-use std::process::exit;
 
 fn main() {
-    let mut numbers = HashSet::<i32>::new();
     let file = read_to_string("../adventofcode_202001.txt").unwrap();
+    let mut numbers = BTreeSet::<i32>::new();
     for f in file.lines() {
         let n = f.parse::<i32>().unwrap();
         numbers.insert(n);
     }
-    for n in &numbers {
-        let n1 = 2020 - n;
-        if numbers.contains(&n1) {
-            println!("{}", n * n1);
-            break;
-        }
-    }
-    for n in &numbers {
-        for n1 in &numbers {
+    let n1 = numbers
+        .iter()
+        .filter(|&n| numbers.contains(&(2020 - *n)))
+        .map(|&n| n * (2020 - n))
+        .next()
+        .unwrap();
+    println!("{n1}");
+    'l: for &n in numbers.iter() {
+        for &n1 in numbers.iter() {
             let n2 = 2020 - n - n1;
             if numbers.contains(&n2) {
                 println!("{}", n * n1 * n2);
-                exit(0);
+                break 'l;
             }
         }
     }
