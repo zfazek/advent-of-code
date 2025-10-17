9b0b4ff956af1b19c0423b966e9628883f1d1eeb
Zoltan Fazekas <zoltan.fazekas@ericsson.com>
2025-10-17 17:08:41 +0200

    update 2024 05 rust solution

--- 2024/05/rust/src/main.rs
+++ 2024/05/rust/src/main.rs
@@ -1,68 +1,51 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn main() {
     let input = std::fs::read_to_string("../input.txt").unwrap();
-    let mut result1: i64 = 0;
-    let mut result2: i64 = 0;
-    let (input_rules, update) = input.split_once("\n\n").unwrap();
-    let rules: Vec<(i64, i64)> = input_rules
+    let (rules_str, updates_str) = input.split_once("\n\n").unwrap();
+    let rules: HashSet<(i64, i64)> = rules_str
         .lines()
-        .map(|line| line.split_once('|').unwrap())
-        .map(|p| (p.0.parse::<i64>().unwrap(), p.1.parse::<i64>().unwrap()))
+        .map(|line| {
+            let (a, b) = line.split_once('|').unwrap();
+            (a.parse().unwrap(), b.parse().unwrap())
+        })
         .collect();
-    for line in update.lines() {
-        let mut tokens: Vec<i64> = line.split(',').map(|x| x.parse::<i64>().unwrap()).collect();
-        let m: HashMap<i64, usize> = tokens
-            .iter()
-            .enumerate()
-            .map(|e| return (e.1.clone(), e.0))
-            .collect();
-        let mut good = true;
-        for &n in tokens.iter() {
-            if !is_good(n, &m, &rules) {
-                good = false;
-                break;
-            }
-        }
-        if good {
-            result1 += tokens[tokens.len() / 2];
+
+    let (mut result1, mut result2) = (0, 0);
+    for line in updates_str.lines() {
+        let pages: Vec<i64> = line.split(',').map(|x| x.parse().unwrap()).collect();
+        let mid = pages[pages.len() / 2];
+
+        if is_valid(&pages, &rules) {
+            result1 += mid;
         } else {
-            loop {
-                let mut good = true;
-                for i in 0..tokens.len() - 1 {
-                    for j in i + 1..tokens.len() {
-                        let a = tokens[i];
-                        let b = tokens[j];
-                        for rule in rules.iter() {
-                            if a == rule.1 && b == rule.0 {
-                                let t = a;
-                                tokens[i] = b;
-                                tokens[j] = t;
-                                good = false;
-                                break;
-                            }
-                        }
-                    }
-                }
-                if good {
-                    result2 += tokens[tokens.len() / 2];
-                    break;
-                }
-            }
+            result2 += sort_pages(&pages, &rules)[pages.len() / 2];
         }
     }
-    println!("{result1}");
-    println!("{result2}");
+    println!("{result1}\n{result2}");
 }
 
-fn is_good(n: i64, m: &HashMap<i64, usize>, rules: &Vec<(i64, i64)>) -> bool {
-    for rule in rules {
-        if n == rule.0 && m.contains_key(&rule.1) && m.get(&rule.0) > m.get(&rule.1) {
-            return false;
-        }
-        if n == rule.1 && m.contains_key(&rule.0) && m.get(&rule.0) > m.get(&rule.1) {
-            return false;
+fn is_valid(pages: &[i64], rules: &HashSet<(i64, i64)>) -> bool {
+    for i in 0..pages.len() {
+        for j in i + 1..pages.len() {
+            if rules.contains(&(pages[j], pages[i])) {
+                return false;
+            }
         }
     }
     true
+}
+
+fn sort_pages(pages: &[i64], rules: &HashSet<(i64, i64)>) -> Vec<i64> {
+    let mut result = pages.to_vec();
+    result.sort_by(|&a, &b| {
+        if rules.contains(&(a, b)) {
+            std::cmp::Ordering::Less
+        } else if rules.contains(&(b, a)) {
+            std::cmp::Ordering::Greater
+        } else {
+            std::cmp::Ordering::Equal
+        }
+    });
+    result
 }
