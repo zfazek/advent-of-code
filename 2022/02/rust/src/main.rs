d3cbd8a75a3d0bc860a6dbb71a98b77917aaae1f
zfazek <ongroa@gmail.com>
2025-10-27 17:47:06 +0100

    update 2022 02 rust solution

--- 2022/02/rust/src/main.rs
+++ 2022/02/rust/src/main.rs
@@ -5,37 +5,30 @@ fn main() {
 }
 
 fn one(file: &str) {
-    let mut sum = 0;
-    for line in file.lines() {
-        let l: Vec<char> = line.chars().collect();
-        let a = l[0] as i32 - 'A' as i32;
-        let b = l[2] as i32 - 'X' as i32;
-        sum += b + 1;
-        if a == b {
-            sum += 3;
-        } else {
-            if b == 0 && a == 2 {
-                sum += 6;
-                continue;
-            } 
-            if b == 1 && a == 0 {
-                sum += 6;
-                continue;
+    let sum: i32 = file
+        .lines()
+        .map(|line| {
+            let l = line.as_bytes();
+            let a = (l[0] - b'A') as i32;
+            let b = (l[2] - b'X') as i32;
+            if a == b {
+                b + 4
+            } else if b == 0 && a == 2 || b == 1 && a == 0 || b == 2 && a == 1 {
+                b + 7
+            } else {
+                b + 1
             }
-            if b == 2 && a == 1 {
-                sum += 6;
-            }
-        }
-    }
+        })
+        .sum();
     println!("{}", sum);
 }
 
 fn two(file: &str) {
     let mut sum = 0;
     for line in file.lines() {
-        let l: Vec<char> = line.chars().collect();
-        let a = l[0] as i32 - 'A' as i32;
-        let mut b = l[2] as i32 - 'X' as i32;
+        let l = line.as_bytes();
+        let a = (l[0] - b'A') as i32;
+        let mut b = (l[2] - b'X') as i32;
         if b == 0 {
             if a == 0 {
                 b = 2;
@@ -58,18 +51,8 @@ fn two(file: &str) {
         sum += b + 1;
         if a == b {
             sum += 3;
-        } else {
-            if b == 0 && a == 2 {
-                sum += 6;
-                continue;
-            } 
-            if b == 1 && a == 0 {
-                sum += 6;
-                continue;
-            }
-            if b == 2 && a == 1 {
-                sum += 6;
-            }
+        } else if b == 0 && a == 2 || b == 1 && a == 0 || b == 2 && a == 1 {
+            sum += 6;
         }
     }
     println!("{}", sum);
