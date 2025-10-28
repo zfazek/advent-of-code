92a07c554e038228f8f4eec30b870bcf74beeba4
zfazek <ongroa@gmail.com>
2025-10-28 13:50:28 +0100

    update 2022 05 rust solution

--- 2022/05/rust/src/main.rs
+++ 2022/05/rust/src/main.rs
@@ -1,30 +1,19 @@
-fn print(stacks: &Vec<Vec<char>>) {
-    for i in 0..9 {
-        if stacks[i].is_empty() {
-            continue;
-        }
-        let v = stacks[i][stacks[i].len() - 1];
-        print!("{}", v);
-    }
-    println!("");
-}
+const SIZE: usize = 9;
 
 fn main() {
     let f = std::fs::read_to_string("../05.txt").unwrap();
-    let mut stacks1: Vec<Vec<char>> = Vec::new();
-    let mut stacks2: Vec<Vec<char>> = Vec::new();
-    for _ in 0..9 {
-        let v1: Vec<char> = Vec::new();
-        let v2: Vec<char> = Vec::new();
-        stacks1.push(v1);
-        stacks2.push(v2);
+    let mut stacks1 = Vec::new();
+    let mut stacks2 = Vec::new();
+    for _ in 0..SIZE {
+        stacks1.push(Vec::new());
+        stacks2.push(Vec::new());
     }
     let mut first_part = true;
     for line in f.lines() {
         let l: Vec<char> = line.chars().collect();
         if l.is_empty() {
             first_part = false;
-            for i in 0..9 {
+            for i in 0..SIZE {
                 stacks1[i].reverse();
                 stacks2[i].reverse();
             }
@@ -34,7 +23,7 @@ fn main() {
             if l[1] == '1' {
                 continue;
             }
-            for i in 0..9 {
+            for i in 0..SIZE {
                 let idx = i * 4 + 1;
                 if idx > l.len() {
                     break;
@@ -48,24 +37,27 @@ fn main() {
             continue;
         }
         if l[0] == 'm' {
-            let mut ll = line.split(" ");
-            ll.next();
-            let n = ll.next().unwrap().parse::<i32>().unwrap();
-            ll.next();
-            let from = ll.next().unwrap().parse::<usize>().unwrap() - 1;
-            ll.next();
-            let to = ll.next().unwrap().parse::<usize>().unwrap() - 1;
+            let ll: Vec<&str> = line.split(" ").collect();
+            let (n, from, to) = (
+                ll[1].parse::<usize>().unwrap(),
+                ll[3].parse::<usize>().unwrap() - 1,
+                ll[5].parse::<usize>().unwrap() - 1,
+            );
             for _ in 0..n {
                 let v = stacks1[from].pop().unwrap();
                 stacks1[to].push(v);
             }
             for i in (1..=n).rev() {
-                let idx = stacks2[from].len() - i as usize;
+                let idx = stacks2[from].len() - i;
                 let v = stacks2[from].remove(idx);
                 stacks2[to].push(v);
             }
         }
     }
-    print(&stacks1);
-    print(&stacks2);
+    println!("{}", get_tops(&stacks1));
+    println!("{}", get_tops(&stacks2));
+}
+
+fn get_tops(stacks: &[Vec<char>]) -> String {
+    stacks.iter().take(SIZE).filter_map(|x| x.last()).collect()
 }
