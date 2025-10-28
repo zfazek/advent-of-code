11bc185c25d808186fad740f251cca6ca826b44d
zfazek <ongroa@gmail.com>
2025-10-28 11:58:24 +0100

    update 2022 04 rust solution

--- 2022/04/rust/src/main.rs
+++ 2022/04/rust/src/main.rs
@@ -1,24 +1,24 @@
 fn main() {
     let file = std::fs::read_to_string("../04.txt").unwrap();
-    let mut sum1 = 0;
-    let mut sum2 = 0;
-    for line in file.lines() {
-        let mut tokens = line.split(",");
-        let first = tokens.next().unwrap();
-        let mut t1 = first.split("-");
-        let a1 = t1.next().unwrap().parse::<i32>().unwrap();
-        let a2 = t1.next().unwrap().parse::<i32>().unwrap();
-        let second = tokens.next().unwrap();
-        let mut t2 = second.split("-");
-        let b1 = t2.next().unwrap().parse::<i32>().unwrap();
-        let b2 = t2.next().unwrap().parse::<i32>().unwrap();
-        if a1 <= b1 && a2 >= b2 || b1 <= a1 && b2 >= a2{
-            sum1 += 1;
+    let (sum1, sum2) = file.lines().fold((0, 0), |(mut acc1, mut acc2), line| {
+        let (first, second) = line.split_once(",").unwrap();
+        let (a1, a2) = parse_token(first);
+        let (b1, b2) = parse_token(second);
+        if a1 <= b1 && a2 >= b2 || b1 <= a1 && b2 >= a2 {
+            acc1 += 1;
         }
-        if a1 <= b2 && a2 >= b1 || b1 <= a2 && b2 >= a1 {
-            sum2 += 1;
+        if a1 <= b2 && a2 >= b1 {
+            acc2 += 1;
         }
-    }
+        (acc1, acc2)
+    });
     println!("{}", sum1);
     println!("{}", sum2);
+}
+
+fn parse_token(first: &str) -> (i32, i32) {
+    let (a1, a2) = first.split_once("-").unwrap();
+    let a1 = a1.parse::<i32>().unwrap();
+    let a2 = a2.parse::<i32>().unwrap();
+    (a1, a2)
 }
