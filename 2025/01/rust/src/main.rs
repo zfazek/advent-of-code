b12dca2293b896d06ad2e261b1a54af8267cdb3e
zfazek <ongroa@gmail.com>
2025-12-01 21:51:31 +0100

    add 2025 01 rust solution

---
+++ 2025/01/rust/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let input = include_str!("../../input.txt");
+    let (count1, count2, _) =
+        input
+            .lines()
+            .fold((0, 0, 50), |(mut count1, mut count2, mut dial), line| {
+                if let Some(c) = line.chars().next()
+                    && let Ok(v) = line[1..].parse::<i32>()
+                {
+                    let sign = if c == 'L' { -1 } else { 1 };
+                    for _ in 0..v {
+                        dial += sign;
+                        if dial % 100 == 0 {
+                            count2 += 1;
+                        }
+                    }
+                    if dial % 100 == 0 {
+                        count1 += 1;
+                    }
+                }
+                (count1, count2, dial % 100)
+            });
+    println!("{count1}");
+    println!("{count2}");
+}
