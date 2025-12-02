d39a772d8148fdcfb547e8e21db9d323b23230f3
zfazek <ongroa@gmail.com>
2025-12-02 20:00:46 +0100

    update 2025 02 rust solution

--- 2025/02/rust/src/main.rs
+++ 2025/02/rust/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let input = include_str!("../../input.txt");
     let tokens = input.split(',');
-    let mut count1: i64 = 0;
-    let mut count2: i64 = 0;
+    let mut count1: usize = 0;
+    let mut count2: usize = 0;
     for token in tokens {
         if let Some((a, b)) = token.split_once("-")
-            && let Ok(a) = a.trim().parse::<i64>()
-            && let Ok(b) = b.trim().parse::<i64>()
+            && let Ok(a) = a.trim().parse::<usize>()
+            && let Ok(b) = b.trim().parse::<usize>()
         {
             for n in a..=b {
                 let n_str = n.to_string();
@@ -19,20 +19,16 @@ fn main() {
     println!("{count2}");
 }
 
-fn get_invalid_id_value(n: i64, n_str: &str, max_len: usize) -> i64 {
+fn get_invalid_id_value(n: usize, n_str: &str, max_len: usize) -> usize {
     let len = n_str.len();
-    for num_token in 2..=max_len {
-        if !len.is_multiple_of(num_token) {
-            continue;
-        }
-        let l = len / num_token;
-        let first_token = &n_str[..l];
-        let found = (1..num_token)
-            .map(|i| &n_str[i * l..(i + 1) * l])
-            .all(|token| token == first_token);
-        if found {
-            return n;
-        }
-    }
-    0
+    (2..=max_len)
+        .filter(|num_token| len.is_multiple_of(*num_token))
+        .any(|num_token| {
+            let l = len / num_token;
+            let first_token = &n_str[..l];
+            (1..num_token)
+                .map(|i| &n_str[i * l..(i + 1) * l])
+                .all(|token| token == first_token)
+        }) as usize
+        * n
 }
