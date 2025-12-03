d841331ddea7bc96ae317f23d68d7fd9ad762d8d
zfazek <ongroa@gmail.com>
2025-12-03 13:05:12 +0100

    add 2025 03 rust solution

---
+++ 2025/03/rust/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    [2, 12]
+        .iter()
+        .map(|&n| {
+            include_str!("../../input.txt")
+                .lines()
+                .map(|line| {
+                    let mut idx = 0;
+                    (0..n)
+                        .rev()
+                        .map(|i| {
+                            let slice = &line[idx..line.len() - i];
+                            let max_digit = slice.chars().max().unwrap();
+                            idx = slice.find(max_digit).unwrap() + idx + 1;
+                            max_digit
+                        })
+                        .collect::<String>()
+                        .parse::<usize>()
+                        .unwrap()
+                })
+                .sum::<usize>()
+        })
+        .for_each(|v| println!("{v}"));
+}
