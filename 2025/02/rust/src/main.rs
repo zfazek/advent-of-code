bc9393077ef821242e0eb415c54bc4923d5e0297
zfazek <ongroa@gmail.com>
2025-12-03 22:43:40 +0100

    update 2025 02 rust solution

--- 2025/02/rust/src/main.rs
+++ 2025/02/rust/src/main.rs
@@ -1,34 +1,32 @@
 fn main() {
-    let input = include_str!("../../input.txt");
-    let tokens = input.split(',');
-    let mut count1: usize = 0;
-    let mut count2: usize = 0;
-    for token in tokens {
-        if let Some((a, b)) = token.split_once("-")
-            && let Ok(a) = a.trim().parse::<usize>()
-            && let Ok(b) = b.trim().parse::<usize>()
-        {
-            for n in a..=b {
-                let n_str = n.to_string();
-                count1 += get_invalid_id_value(n, &n_str, 2);
-                count2 += get_invalid_id_value(n, &n_str, n_str.len());
-            }
-        }
-    }
+    let (count1, count2) =
+        include_str!("../../input.txt")
+            .split(',')
+            .fold((0, 0), |(acc1, acc2), token| {
+                let (a, b) = token.split_once("-").unwrap();
+                let (v1, v2) = (a.trim().parse::<usize>().unwrap()
+                    ..=b.trim().parse::<usize>().unwrap())
+                    .map(|n| {
+                        (
+                            get_invalid_id_value(n, &n.to_string(), 2),
+                            get_invalid_id_value(n, &n.to_string(), n.to_string().len()),
+                        )
+                    })
+                    .reduce(|(a1, a2), (x, y)| (a1 + x, a2 + y))
+                    .unwrap();
+                (acc1 + v1, acc2 + v2)
+            });
     println!("{count1}");
     println!("{count2}");
 }
 
 fn get_invalid_id_value(n: usize, n_str: &str, max_len: usize) -> usize {
-    let len = n_str.len();
     (2..=max_len)
-        .filter(|num_token| len.is_multiple_of(*num_token))
+        .filter(|num_token| n_str.len().is_multiple_of(*num_token))
         .any(|num_token| {
-            let l = len / num_token;
-            let first_token = &n_str[..l];
             (1..num_token)
-                .map(|i| &n_str[i * l..(i + 1) * l])
-                .all(|token| token == first_token)
+                .map(|i| &n_str[i * (n_str.len() / num_token)..(i + 1) * (n_str.len() / num_token)])
+                .all(|token| token == &n_str[..n_str.len() / num_token])
         }) as usize
         * n
 }
