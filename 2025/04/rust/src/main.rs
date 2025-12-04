fe7029327185fc3bfd561633c821beba33ff71bc
zfazek <ongroa@gmail.com>
2025-12-04 14:44:06 +0100

    update 2025 04 rust solution

--- 2025/04/rust/src/main.rs
+++ 2025/04/rust/src/main.rs
@@ -1,52 +1,52 @@
 fn main() {
-    let vv_orig: Vec<Vec<_>> = include_str!("../../input.txt")
+    let grid_orig: Vec<Vec<_>> = include_str!("../../input.txt")
         .lines()
         .map(|line| line.chars().collect())
         .collect();
-    let mut vv_new = vv_orig.clone();
+    let mut grid_new = grid_orig.clone();
     let mut result1 = 0;
-    calculate(&mut vv_new, &vv_orig, &mut result1);
+    calculate(&mut grid_new, &grid_orig, &mut result1);
     println!("{result1}");
 
-    vv_new = vv_orig.clone();
-    let mut vv_prev = vv_orig.clone();
+    grid_new = grid_orig.clone();
+    let mut grid_prev = grid_orig.clone();
     let mut result2 = 0;
     loop {
-        if !calculate(&mut vv_new, &vv_prev, &mut result2) {
+        if !calculate(&mut grid_new, &grid_prev, &mut result2) {
             break;
         }
-        vv_prev = vv_new.clone();
+        grid_prev = grid_new.clone();
     }
     println!("{result2}");
 }
 
-fn calculate(vv_new: &mut [Vec<char>], vv_prev: &[Vec<char>], result: &mut i32) -> bool {
+fn calculate(grid_new: &mut [Vec<char>], grid_prev: &[Vec<char>], changes: &mut i32) -> bool {
     let mut found = false;
-    for i in 0..vv_new.len() {
-        for j in 0..vv_new[0].len() {
-            if vv_prev[i][j] != '@' {
+    let len = grid_new.len();
+    for i in 0..len {
+        for j in 0..len {
+            if grid_prev[i][j] != '@' {
                 continue;
             }
             let mut count = 0;
-            for dj in -1_i32..2 {
-                for di in -1_i32..2 {
+            for dj in -1..2 {
+                for di in -1..2 {
                     if di == 0 && dj == 0 {
                         continue;
                     }
-                    let ii = i as i32 + di;
-                    let jj = j as i32 + dj;
-                    if ii < 0 || ii >= vv_new.len() as i32 || jj < 0 || jj >= vv_new[0].len() as i32
-                    {
+                    let new_i = i as i32 + di;
+                    let new_j = j as i32 + dj;
+                    if new_i < 0 || new_i >= len as i32 || new_j < 0 || new_j >= len as i32 {
                         continue;
                     }
-                    if vv_prev[ii as usize][jj as usize] == '@' {
+                    if grid_prev[new_i as usize][new_j as usize] == '@' {
                         count += 1;
                     }
                 }
             }
             if count < 4 {
-                vv_new[i][j] = '.';
-                *result += 1;
+                grid_new[i][j] = '.';
+                *changes += 1;
                 found = true;
             }
         }
