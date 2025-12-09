974cd5f1a0703a24722a56a591ccc82857a9aca2
zfazek <ongroa@gmail.com>
2025-12-09 16:41:49 +0100

    update 2025 09 rust solution

--- 2025/09/rust/src/main.rs
+++ 2025/09/rust/src/main.rs
@@ -1,20 +1,21 @@
 fn main() {
-    println!(
-        "{}",
-        include_str!("../../input.txt")
-            .lines()
-            .map(|line| line.split_once(",").unwrap())
-            .map(|(x, y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()))
-            .enumerate()
-            .map(|(i, pi)| include_str!("../../input.txt")
+    let input = include_str!("../../input.txt");
+    let result1 = input
+        .lines()
+        .map(|line| line.split_once(",").unwrap())
+        .map(|(x, y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()))
+        .enumerate()
+        .map(|(i, pi)| {
+            input
                 .lines()
                 .map(|line| line.split_once(",").unwrap())
                 .map(|(x, y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()))
                 .skip(i + 1)
                 .map(|pj| (pi.0 - pj.0 + 1) * (pi.1 - pj.1 + 1))
                 .max()
-                .unwrap_or(0))
-            .max()
-            .unwrap()
-    );
+                .unwrap_or(0)
+        })
+        .max()
+        .unwrap();
+    println!("{}", result1);
 }
