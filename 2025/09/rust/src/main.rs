b70a20a1e7a737152a3f53c8ff7be2ceddb7ad9a
zfazek <ongroa@gmail.com>
2025-12-10 05:59:32 +0100

    update 2025 09 rust solution

--- 2025/09/rust/src/main.rs
+++ 2025/09/rust/src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
-    let input = include_str!("../../input.txt");
-    let result1 = input
+    let ps = include_str!("../../input.txt")
         .lines()
         .map(|line| line.split_once(",").unwrap())
-        .map(|(x, y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()))
+        .map(|(x, y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()));
+    let result1 = ps
+        .clone()
         .enumerate()
         .map(|(i, pi)| {
-            input
-                .lines()
-                .map(|line| line.split_once(",").unwrap())
-                .map(|(x, y)| (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap()))
+            ps.clone()
                 .skip(i + 1)
                 .map(|pj| (pi.0 - pj.0 + 1) * (pi.1 - pj.1 + 1))
                 .max()
